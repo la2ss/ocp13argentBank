e0b2870542faab58249d0e5228bafe9a5a23b402
Lassina Sylla <lass390225@gmail.com>
2023-11-22 13:22:23 +0100

    add page home et 404

---
+++ src/pages/error/Error.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <div className="container">
+        <h2>404</h2>
+        <p>
+
+          Oups! Nous rencontrons des difficultés à charger la page
+          demandée.
+        </p>
+
+      </div>
+    </main>
+
+  );
+}
